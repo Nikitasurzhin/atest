4a62748fbb8c31214df826a5d8c18bc9aa1ab0c1
n.surzhin <1>
2024-01-22 15:10:37 +0300

    Доработка NavigationMenuTest и TemplatesMenuTest

--- src/test/java/org.eltpoisk/TemplatesMenuTest.java
+++ src/test/java/org.eltpoisk/TemplatesMenuTest.java
@@ -14,22 +14,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TemplatesMenuTest {
     WebDriver driver;
     public static LoginPage loginPage;
     public static ProfilePage profilePage;
-
-    String textConverter( String in){
-        try {
-            return new String (in.getBytes("UTF-8" ),  Charset.forName("windows-1251"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return in;
-    }
 
     @BeforeAll
     static void setupAll() {
@@ -49,7 +45,7 @@ public class TemplatesMenuTest {
     }
 
     @Test
-    public void navigationMenuTest() throws InterruptedException {
+    public void TemplatesMenuTestFilter() throws InterruptedException {
 
         //Arrange
         // подготовительный действия
@@ -67,34 +63,52 @@ public class TemplatesMenuTest {
         loginPage.inputPasswd(ConfProperties.getProperty("passwd"));
         //нажимаем кнопку входа
         loginPage.clickLoginBtn();
-
+        WebElement PageOne = driver.findElement(By.xpath("//*[@id=\"ext-comp-1016\"]"));
+        String loginPageOne = PageOne.getText();
         profilePage.Template();
-
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"ext-gen3\"]/table/tbody/tr[1]/td/a/img")).isDisplayed()));
-        assertEquals("Фамилия154646 И.О.", textConverter(driver.findElement(By.xpath("//*[@id=\"ext-gen3\"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/span[1]")).getText()));
-        assertEquals("0", textConverter(driver.findElement(By.xpath("//*[@id=\"ext-gen3\"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/span[2]/strong")).getText()));
+        assertEquals(loginPageOne, (driver.findElement(By.xpath("//*[@id=\"ext-gen3\"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/span[1]")).getText()));
+        System.out.println(loginPageOne);
+        assertEquals("0", (driver.findElement(By.xpath("//*[@id=\"ext-gen3\"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/span[2]/strong")).getText()));
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"ext-gen3\"]/table/tbody/tr[2]/td/table/tbody/tr[1]/td/table/tbody/tr/td[2]")).isDisplayed()));
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[1]/input[1]")).isDisplayed()));
         profilePage.clickCheckboxAll();
-        assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[1]/input[1]")).isSelected()));
+        WebElement checkbox = driver.findElement(By.cssSelector("input[name='to_del[]']"));
+        assertEquals(true, checkbox.isSelected());
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"to_del_all\"]")).isDisplayed()));
         profilePage.CheckDelAll();
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"to_del_all\"]")).isSelected()));
-        assertEquals("с ", textConverter(driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[2]/table/tbody/tr/td[1]")).getText()));
+        assertEquals("с ", (driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[2]/table/tbody/tr/td[1]")).getText()));
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"date_from\"]")).isDisplayed()));
         profilePage.clickDateFieldsForm();
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"calendarDiv\"]")).isDisplayed()));
-        assertEquals(" по ", textConverter(driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[2]/table/tbody/tr/td[3]")).getText()));
+        assertEquals(" по ", (driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[2]/table/tbody/tr/td[3]")).getText()));
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"date_to\"]")).isDisplayed()));
         profilePage.clickDateFieldsTo();
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"calendarDiv\"]")).isDisplayed()));
         profilePage.clickExitCalendare();
         assertEquals(true, (driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[3]/select")).isDisplayed()));
         profilePage.clickFilterUsername();
-        Thread.sleep(3000);
 
+        WebElement dropdownList = driver.findElement(By.xpath("//*[@id=\"saved_contracts_table\"]/thead/tr[2]/th[3]/select"));
 
+        List<WebElement> options = dropdownList.findElements(By.tagName("option"));
+        ArrayList<String> originalList =  new ArrayList<String>();
+        for (WebElement option : options) {
+            originalList.add(option.getText());
+        }
+        ArrayList<String> sortedList = new ArrayList<String>(originalList);
+        Collections.sort(sortedList);
 
+        boolean isSorted = true;
+        for (int i = 0; i < originalList.size(); i++) {
+            if (!originalList.get(i).equals(sortedList.get(i))) {
+                isSorted = false;
+                System.out.println(sortedList);
+                break;
+            }
+        }
+        assertTrue(isSorted);
 
 
 
